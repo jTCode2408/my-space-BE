0f7c60005c4100f98da9220680c2a7c3de2af736
Jasmine Terry <jasmine-terry91@gmail.com>
2020-09-07 21:01:41 -0400

    spotify auth code

--- server.js
+++ server.js
@@ -16,7 +16,44 @@ server.get('/', (req,res)=>{
 });
 
 
+const redirect_uri=
+process.env.REDIRECT_URI || 
+'http://localhost:8888/callback'
+const stateKey='spotify_auth_state';
 
+
+server.get('/login', function(req, res) {
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      scope: 'user-read-recently-played user-top-read playlist-read-private',
+      redirect_uri
+    }))
+})
+
+server.get('/callback', function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (Buffer.from(
+        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    res.redirect(uri + '?access_token=' + access_token)
+  })
+})
 
 
 
