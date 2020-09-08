2743f500cd0cfcab7c776e77929801b0b9258d07
Jasmine Terry <jasmine-terry91@gmail.com>
2020-09-08 18:29:55 -0400

    cors fix

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require ('express')
-const server = express()
 const cors = require('cors')
-const querystring =require('querystring');
+const server = express()
 server.use(cors());
+server.options('*', cors());
 server.use(express.json());
-
+const querystring =require('querystring');
 
 const request = require('request'); // "Request" library
 const cookieParser = require('cookie-parser');
